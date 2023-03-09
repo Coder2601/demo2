50cd9d546af76fd7983f2da526deea30158180e9
Divya Goyal <77169739+Coder2601@users.noreply.github.com>
2023-03-09 15:21:18 +0530

    Add files via upload

---
+++ b4.js
@@ -0,0 +1,77 @@
+// var fname = 'Divya ';
+// var lname = 'Goyal'
+// var fullName = fname.concat(lname);
+// console.log(fullName);
+
+// var len = fname.length
+// console.log(len);
+
+// console.log(fname.toUpperCase());
+// console.log(fname.toLowerCase());
+
+// var fruit = 'pineapple'
+// console.log(fruit.slice(4,9));
+
+// console.log(fullName.charAt(6));
+
+
+// var sen = 'Ram is eating apple. Ram is happy.';
+// console.log(sen);
+
+// var newSen = sen.replace(/ram/gi,'Ravi');
+// console.log(newSen);
+
+
+// var fruits = ['apple','mango','grapes'];
+// console.log(fruits.length);
+
+// console.log(fruits[0]);
+
+// fruits.push('oranges');
+// console.log(fruits);
+
+// fruits.pop();
+// console.log(fruits);
+
+// var thisIsAnArray = ["element1", "element2", "element3", "element4"];
+
+// for(var i =0;i < thisIsAnArray.length;i++){
+//     console.log(thisIsAnArray[i]);
+// }
+
+// var arr=[];
+
+// for(var i=1; i<=5;i++){
+//     var ip = prompt('Enter a name');
+//     arr.push(ip);
+// }
+// console.log(arr);
+
+// var rand = Math.floor(Math.random()*5);
+// console.log(rand);
+
+// console.log(arr[rand] + " is going to buy lunch today..! :)")
+// console.log(`${arr[rand]} is going to buy lunch today..! :)`)
+
+
+
+// var mainArr = [];
+
+
+// for(var i=0;i<4;i++){
+//     if(i<2 || i>2){
+//         var subArr =[];
+//     for(var j=0;j<2;j++){
+//         var ip = prompt('Enter values');
+//         subArr.push(ip);
+//     }
+//     mainArr.push(subArr);
+//     }
+//     else{
+//         var ip = prompt('Enter value');
+//         mainArr.push(ip)
+//     }
+// }
+// console.log(mainArr);
+
+
